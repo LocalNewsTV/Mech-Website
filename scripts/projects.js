98f9e5c86a3c2b41e1aec6c2d3ecdecc8323aca1
Matthew Logan <loganmatthewr@gmail.com>
2023-06-03 13:07:53 -0700

    Tidied Objects, updated template object

--- scripts/projects.js
+++ scripts/projects.js
@@ -1,80 +1,87 @@
 /****************************************************************************
- * 
- * All the projects with their writeups are contained here so new ones can 
- * easily be added with this template object 
- * 
+ *
+ * All the projects with their writeups are contained here so new ones can
+ * easily be added with this template object
+ *
  *****************************************************************************/
 /*
-      {
-          id: "teamA",
-          title: "Team A",
-          href: '#',
-          sourceImage: "https://fakeimg.pl/325x200",
-          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hendrerit turpis in arcu sodales, id convallis sem bibendum. Pellentesque condimentum mollis venenatis."
-      },
+    {
+        id: "team_"
+        title: "Team _"
+        href: '/team_.html"
+    },
  */
-    projects = [
-      {
-          id: "teamA",
-          title: "Team A",
-          href: 'teams/teamtemplate.html',
-          sourceImage: "https://fakeimg.pl/325x200",
-          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hendrerit turpis in arcu sodales, id convallis sem bibendum. Pellentesque condimentum mollis venenatis."
-      },
-      {
-          id: "teamB",
-          title: "Team B",
-          href: 'teams/teamtemplate.html',
-          sourceImage: "https://fakeimg.pl/325x200",
-          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hendrerit turpis in arcu sodales, id convallis sem bibendum. Pellentesque condimentum mollis venenatis."
-      },
-      {
-          id: "teamC",
-          title: "Team C",
-          href: 'teams/teamtemplate.html',
-          sourceImage: "https://fakeimg.pl/325x200",
-          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hendrerit turpis in arcu sodales, id convallis sem bibendum. Pellentesque condimentum mollis venenatis."
-      },
-      {
-          id: "teamD",
-          title: "Team D",
-          href: 'teams/teamtemplate.html',
-          sourceImage: "https://fakeimg.pl/325x200",
-          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hendrerit turpis in arcu sodales, id convallis sem bibendum. Pellentesque condimentum mollis venenatis."
-      },
-      {
-          id: "teamE",
-          title: "Team E",
-          href: 'teams/teamtemplate.html',
-          sourceImage: "https://fakeimg.pl/325x200",
-          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hendrerit turpis in arcu sodales, id convallis sem bibendum. Pellentesque condimentum mollis venenatis."
-      },
-      {
-          id: "teamF",
-          title: "Team F",
-          href: 'teams/teamtemplate.html',
-          sourceImage: "https://fakeimg.pl/325x200",
-          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hendrerit turpis in arcu sodales, id convallis sem bibendum. Pellentesque condimentum mollis venenatis."
-      },
-      {
-          id: "teamG",
-          title: "Team G",
-          href: 'teams/teamtemplate.html',
-          sourceImage: "https://fakeimg.pl/325x200",
-          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hendrerit turpis in arcu sodales, id convallis sem bibendum. Pellentesque condimentum mollis venenatis."
-      },
-      {
-          id: "teamH",
-          title: "Team H",
-          href: 'teams/teamtemplate.html',
-          sourceImage: "https://fakeimg.pl/325x200",
-          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hendrerit turpis in arcu sodales, id convallis sem bibendum. Pellentesque condimentum mollis venenatis."
-      },
-      {
-          id: "teamI",
-          title: "Team I",
-          href: 'teams/teamtemplate.html',
-          sourceImage: "https://fakeimg.pl/325x200",
-          description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hendrerit turpis in arcu sodales, id convallis sem bibendum. Pellentesque condimentum mollis venenatis."
-      },
-  ]
+projects = [
+  {
+    id: "teamA",
+    title: "Team A",
+    href: "teams/teamtemplate.html",
+    sourceImage: "https://fakeimg.pl/325x200",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hendrerit turpis in arcu sodales, id convallis sem bibendum. Pellentesque condimentum mollis venenatis.",
+  },
+  {
+    id: "teamB",
+    title: "Team B",
+    href: "teams/teamtemplate.html",
+    sourceImage: "https://fakeimg.pl/325x200",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hendrerit turpis in arcu sodales, id convallis sem bibendum. Pellentesque condimentum mollis venenatis.",
+  },
+  {
+    id: "teamC",
+    title: "Team C",
+    href: "teams/teamtemplate.html",
+    sourceImage: "https://fakeimg.pl/325x200",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hendrerit turpis in arcu sodales, id convallis sem bibendum. Pellentesque condimentum mollis venenatis.",
+  },
+  {
+    id: "teamD",
+    title: "Team D",
+    href: "teams/teamtemplate.html",
+    sourceImage: "https://fakeimg.pl/325x200",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hendrerit turpis in arcu sodales, id convallis sem bibendum. Pellentesque condimentum mollis venenatis.",
+  },
+  {
+    id: "teamE",
+    title: "Team E",
+    href: "teams/teamtemplate.html",
+    sourceImage: "https://fakeimg.pl/325x200",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hendrerit turpis in arcu sodales, id convallis sem bibendum. Pellentesque condimentum mollis venenatis.",
+  },
+  {
+    id: "teamF",
+    title: "Team F",
+    href: "teams/teamtemplate.html",
+    sourceImage: "https://fakeimg.pl/325x200",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hendrerit turpis in arcu sodales, id convallis sem bibendum. Pellentesque condimentum mollis venenatis.",
+  },
+  {
+    id: "teamG",
+    title: "Team G",
+    href: "teams/teamtemplate.html",
+    sourceImage: "https://fakeimg.pl/325x200",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hendrerit turpis in arcu sodales, id convallis sem bibendum. Pellentesque condimentum mollis venenatis.",
+  },
+  {
+    id: "teamH",
+    title: "Team H",
+    href: "teams/teamtemplate.html",
+    sourceImage: "https://fakeimg.pl/325x200",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hendrerit turpis in arcu sodales, id convallis sem bibendum. Pellentesque condimentum mollis venenatis.",
+  },
+  {
+    id: "teamI",
+    title: "Team I",
+    href: "teams/teamtemplate.html",
+    sourceImage: "https://fakeimg.pl/325x200",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hendrerit turpis in arcu sodales, id convallis sem bibendum. Pellentesque condimentum mollis venenatis.",
+  },
+];
