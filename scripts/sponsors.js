2f431f05bdf4cc6a9607fd1f01b6b62301c6d5ec
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2023-07-10 20:32:48 -0700

    Sponsors added

--- scripts/sponsors.js
+++ scripts/sponsors.js
@@ -4,20 +4,32 @@ Sponsor template page for easy replication/deletion
 
 sponsors = [{
     id: "sponsor1",
-    title: "Sponsor 1",
-    href: "https://www.google.com",
-    sourceImage: "https://fakeimg.pl/325x200"
+    title: "Camosun College",
+    href: "https://www.camosun.ca",
+    sourceImage: "../images/team-sponsors/camosun.png"
   },
   {
     id: "sponsor2",
-    title: "Sponsor 2",
-    href: "https://www.bing.com",
-    sourceImage: "https://fakeimg.pl/325x200"
+    title: "Fossil Beach Farm",
+    href: "https://fossilbeachfarm.com/",
+    sourceImage: "../images/team-sponsors/fossil-beach-farm.jpeg"
   },
   {
-    id: "sponsor3",
-    title: "Sponsor 3",
-    href: "https://www.duckduckgo.com",
-    sourceImage: "https://fakeimg.pl/325x200"
-  }
+    id: "sponsor4",
+    title: "Green Room",
+    href: "https://www.greenroombody.com/",
+    sourceImage: "../images/team-sponsors/greenroom.png"
+  },
+  {
+    id: "sponsor5",
+    title: "Holocene Distilling",
+    href: "#",
+    sourceImage: "../images/team-sponsors/holocene.png"
+  },
+  {
+    id: "sponsor6",
+    title: "Sponsor 2",
+    href: "https://www.nbfwm.ca/advisor/macdougall-wealth-management-group.html",
+    sourceImage: "../images/team-sponsors/nationalbank.jpg"
+  },
 ];
