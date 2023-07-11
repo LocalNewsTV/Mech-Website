75f54cf8eea9593fd94d7c6288f9246968ec83bc
LocalNewsTV <62873746+LocalNewsTV@users.noreply.github.com>
2023-07-11 13:43:52 -0700

    fixed rel. links in sponsor images

--- scripts/sponsors.js
+++ scripts/sponsors.js
@@ -6,30 +6,30 @@ sponsors = [{
     id: "sponsor1",
     title: "Camosun College",
     href: "https://www.camosun.ca",
-    sourceImage: "../images/team-sponsors/camosun.png"
+    sourceImage: "images/team-sponsors/camosun.png"
   },
   {
     id: "sponsor2",
     title: "Fossil Beach Farm",
     href: "https://fossilbeachfarm.com/",
-    sourceImage: "../images/team-sponsors/fossil-beach-farm.jpeg"
+    sourceImage: "images/team-sponsors/fossil-beach-farm.jpeg"
   },
   {
     id: "sponsor4",
     title: "Green Room",
     href: "https://www.greenroombody.com/",
-    sourceImage: "../images/team-sponsors/greenroom.png"
+    sourceImage: "images/team-sponsors/greenroom.png"
   },
   {
     id: "sponsor5",
     title: "Holocene Distilling",
     href: "#",
-    sourceImage: "../images/team-sponsors/holocene.png"
+    sourceImage: "images/team-sponsors/holocene.png"
   },
   {
     id: "sponsor6",
     title: "Sponsor 2",
     href: "https://www.nbfwm.ca/advisor/macdougall-wealth-management-group.html",
-    sourceImage: "../images/team-sponsors/nationalbank.jpg"
+    sourceImage: "images/team-sponsors/nationalbank.jpg"
   },
 ];
